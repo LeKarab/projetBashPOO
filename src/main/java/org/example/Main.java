83ac0d3acecb8d2cb9ee8ee68bea4a30b099ae5e
Romain FOND <rom1.fond@gmail.com>
2024-06-15 21:39:00 +0200

    utilisation de chemins relatifs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Chemin du dossier où se trouvent les fichiers CSV à traiter
-        String inputFolder = "C:\\Users\\georj\\Dropbox\\Mon PC (LAPTOP-D1NNT2HT)\\Documents\\FISA 2\\4_DEV\\POO\\projetBashPOO\\src\\main\\resources\\raw";
+        String inputFolder = "src/main/resources/raw";
         // Chemin du dossier où les fichiers traités seront déplacés
-        String processedFolder = "C:\\Users\\georj\\Dropbox\\Mon PC (LAPTOP-D1NNT2HT)\\Documents\\FISA 2\\4_DEV\\POO\\projetBashPOO\\src\\main\\resources\\processed";
+        String processedFolder = "src/main/resources/processed";
         String dbUrl = "jdbc:postgresql://localhost:5432/projet";
         String dbUser = "postgres";
         String dbPass = "admin";
